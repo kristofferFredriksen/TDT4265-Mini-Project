d0369381b488af64a3f15a159f492a458020524d
Kristoffer Thorsdal Fredriksen <kristotf@stud.ntnu.no>
2026-02-24 15:34:54 +0100

    feat: testscript

---
+++ src/train.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+import torch as t
+
+device = t.device("cuda" if t.cuda.is_available() else "cpu")
+
+#Loading pretrained YOLO on COCO dataset
+print("Loading pretrained YOLO model...")
+model = YOLO("yolo26n.pt")
+print("Model loaded successfully.")
+
+print ("Starting training...")
+results = model.train(data = "./src/data_roadpoles_v1.yaml", epochs=20, batch = 16, imgsz = 640, device="device", verbose = True)
+print("Training completed.")
